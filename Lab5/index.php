e2ecb83f7eba22eb176b55f1fcde02e785912a61
CSU-Ryan <enderbro@colostate.edu>
2026-02-24 11:33:51 -0700

    Adds form

--- Lab5/index.php
+++ Lab5/index.php
@@ -3,11 +3,20 @@
 <html>
 <body>
     <h2>PHP Below:</h2>
+
     <div id='main'>
         <?php
         $greeting = "Hello World!";
         echo $greeting;
         ?>
-    <div>
+    </div>
+
+    <form method='POST'>
+        <h2>Please input your name:</h2>
+
+        <input type="text" name="name">
+
+        <input type="submit" value="Submit Name">
+     </form>
 </body>
 </html>

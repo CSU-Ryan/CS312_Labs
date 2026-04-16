0caf6f8f256f07a4b429e9352da210ec94e4cc76
CSU-Ryan <enderbro@colostate.edu>
2026-04-16 11:39:22 -0600

    Adds removal of old variables

--- Lab_SQL/enderbro.sql
+++ Lab_SQL/enderbro.sql
@@ -1,3 +1,12 @@
+-- REMOVE HANGING VARIABLES
+DROP TABLE IF EXISTS enderbro_students;
+DROP TABLE IF EXISTS enderbro_classes;
+DROP TABLE IF EXISTS enderbro_enrollments;
+
+DROP PROCEDURE IF EXISTS add_students();
+DROP PROCEDURE IF EXISTS enroll_students();
+
+
 -- STUDENTS TABLE
 CREATE TABLE enderbro_students (
     name VARCHAR(255),

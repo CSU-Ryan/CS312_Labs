979a9e55311955a8f223cc26c89fddca95517383
CSU-Ryan <enderbro@colostate.edu>
2026-04-18 21:17:09 -0600

    Fixes DROP TABLE?

--- Lab_SQL/enderbro.sql
+++ Lab_SQL/enderbro.sql
@@ -1,10 +1,10 @@
 -- REMOVE HANGING VARIABLES
-DROP TABLE IF EXISTS 'enderbro_students';
-DROP TABLE IF EXISTS 'enderbro_classes';
-DROP TABLE IF EXISTS 'enderbro_enrollments';
-
-DROP PROCEDURE IF EXISTS add_students;
 DROP PROCEDURE IF EXISTS enroll_students;
+DROP PROCEDURE IF EXISTS add_students;
+
+DROP TABLE IF EXISTS enderbro_enrollments;
+DROP TABLE IF EXISTS enderbro_classes;
+DROP TABLE IF EXISTS enderbro_students;
 
 
 -- STUDENTS TABLE
